0d48e33c610137327512a8a6248d06c3e7388845
mlsagar <sagar.maharjan617571@outlook.com>
2026-02-05 17:26:04 -0500

    Add: solution of Object.js polyfill

---
+++ types-exercises/object-is/main.js
@@ -0,0 +1,33 @@
+if (!Object.is) {
+    Object.is = function ObjectIs(param1, param2) {
+        if (param1 === 0 && param2 === 0) {
+            param1 = 1 / param1;
+            param2 = 1 / param2;
+        } else if (Number.isNaN(param1) && Number.isNaN(param2)) {
+            return true;
+        }
+
+        return param1 === param2;
+        // const param1ToString = param1.toString()
+    }
+}
+
+console.log(Object.is(42,42) === true);
+console.log(Object.is("foo","foo") === true);
+console.log(Object.is(false,false) === true);
+console.log(Object.is(null,null) === true);
+console.log(Object.is(undefined,undefined) === true);
+console.log(Object.is(NaN,NaN) === true);
+console.log(Object.is(-0,-0) === true);
+console.log(Object.is(0,0) === true);
+
+console.log(Object.is(-0,0) === false);
+console.log(Object.is(0,-0) === false);
+console.log(Object.is(0,NaN) === false);
+console.log(Object.is(NaN,0) === false);
+console.log(Object.is(42,"42") === false);
+console.log(Object.is("42",42) === false);
+console.log(Object.is("foo","bar") === false);
+console.log(Object.is(false,true) === false);
+console.log(Object.is(null,undefined) === false);
+console.log(Object.is(undefined,null) === false);
